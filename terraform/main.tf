b5894af340fa27a2df656f56d419fd085bc37eb8
stadeof <krasichkov8962@gmail.com>
2023-02-06 16:50:53 +0300

    add: homework 2, terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,14 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
-
 provider "yandex" {
     cloud_id = "b1gagaienr53r9872vr0"
     folder_id = var.folder_id
     zone = var.zone
 }
 
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
-}
+resource "yandex_compute_instance" "vm" {
 
-resource "yandex_compute_instance" "node01" {
-
-    name        = "node01"
-    hostname    = "node01"
-    description = "my test node"
+    name        = "node"
+    hostname    = "node"
+    description = "node"
     folder_id   = var.folder_id
     zone        = var.zone
     platform_id = "standard-v2"
@@ -32,13 +18,13 @@ resource "yandex_compute_instance" "node01" {
     resources {
         cores         = 2
         core_fraction = 100
-        memory        = 4
+        memory        = 2
     }
 
     boot_disk {
         initialize_params {
-            image_id = "fd8uc1vvpjqho0i9skl9"
-            size     = 30 
+            image_id = "fd864gbboths76r8gm5f"
+            size     = 15 
             type     = "network-ssd"
         }
     }
@@ -49,12 +35,3 @@ resource "yandex_compute_instance" "node01" {
     }
 }
 
-variable "folder_id" {
-    type    = string 
-    default = "b1gavj4e750dodf8ajlg"
-}
-
-variable "zone" {
-    type    = string
-    default = "ru-central1-a"
-}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "folder_id" {
+    type    = string 
+    default = "b1gavj4e750dodf8ajlg"
+}
+
+variable "zone" {
+    type    = string
+    default = "ru-central1-a"
+}
